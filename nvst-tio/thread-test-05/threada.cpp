f294ac75979c230bbb89a02ca8479c43a549ad91
chd1998 <chd@ynao.ac.cn>
2022-04-03 12:40:43 +0800

    thread-test-05 using real 16bit data and vectors

---
+++ nvst-tio/thread-test-05/threada.cpp
@@ -0,0 +1,60 @@
+#include "threada.h"
+#include "mainwindow.h"
+
+threadA::threadA()
+{
+    countA=0;
+    src="A";
+
+}
+
+void threadA::working()
+{
+    grayimage16 = new QImage(imgX,imgY,QImage::Format_Grayscale16);
+    grayimage = new QImage(imgX,imgY,QImage::Format_Indexed8);
+    myImage= new unsigned short[imgX*imgY];
+    myImageBack=myImage;
+
+    while(!QThread::currentThread()->isInterruptionRequested())
+    {
+
+        lockA.lock();
+        int row=imgY;
+        int col=imgX;
+       //generate random data
+        for(int i = 0 ;i < row ;i++)
+        {
+            for(int j=0;j< col;j++)
+            {
+                //myImage[j+i*row]=(unsigned short)QRandomGenerator::global()->bounded(0,65535);
+                myImage[j+i*row]=QRandomGenerator::global()->bounded(20000,42000);
+                vecimg.at(j+i*row)=myImage[j+i*row];
+            }
+        }
+        emit fromA(src,countA);
+        if(!imglocked || !histlocked)
+        {
+            //std::vector<unsigned short> vecimg(myImage,myImage+sizeof(*myImage)/sizeof(myImage[0]));
+            //std::vector<unsigned short> vecimg(sizeof(*myImage)/sizeof(myImage[0]));
+            //memcpy(&vecimg[0], myImage, sizeof(*myImage));
+            //qDebug()<<"img size: "<<vecimg.size()<<" Value: "<<vecimg.at(32768);
+            emit imgReady();
+        }
+        countA++;
+        lockA.unlock();
+        //qDebug()<<"inside A-03";
+        QElapsedTimer t;
+        t.start();
+        while(t.elapsed()<frameRate)
+            QCoreApplication::processEvents();
+
+    }
+    delete grayimage16;
+    delete grayimage;
+    delete[] myImageBack;
+}
+
+//void threadA::finished()
+//{
+
+//}
